cfdbe74e7e0bc0d194ca48b692a9982481871e7f
Zewei Chen <czwking1991@gmail.com>
2015-01-14 17:06:10 +0800

    version 1

---
+++ client/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket server, client;
+        private IPEndPoint ipEnd;
+        byte[] recv;
+        int i;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            recv = new byte[200];
+            ipEnd = new IPEndPoint(IPAddress.Parse(IP.Text), 15000);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipEnd);
+            }
+            catch(SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            //while(true)
+            {
+                try
+                {
+                    i = server.Receive(recv);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                Result.AppendText( Encoding.UTF8.GetString(recv,0,i)+"\n");
+            }
+        }
+    }
+}
